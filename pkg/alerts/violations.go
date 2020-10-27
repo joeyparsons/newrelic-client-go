1dbc2602e8a65ea68899abb96fba7098e2f3ff41
joeyparsons <joey.parsons@gmail.com>
2020-10-27 13:29:49 -0700

    [violations] add new methods

---
+++ pkg/alerts/violations.go
@@ -0,0 +1,53 @@
+package alerts
+
+type AlertViolationEntity struct {
+	Product string `json:"product"`
+	Type    string `json:"type"`
+	GroupId string `json:"group_id"`
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+}
+
+type AlertViolation struct {
+	ID            int                  `json:"id"`
+	Label         string               `json:"label"`
+	Duration      int                  `json:"duration"`
+	PolicyName    string               `json:"policy_name"`
+	ConditionName string               `json:"condition_name"`
+	Priority      string               `json:"priority"`
+	OpenedAt      int                  `json:"opened_at"`
+	ClosedAt      int                  `json:"closed_at"`
+	Entity        AlertViolationEntity `json:"entity"`
+}
+
+// ListAlertEventsParams represents a set of filters to be used
+// when querying New Relic alert events
+type ListAlertViolationsParams struct {
+	Page int `url:"page,omitempty"`
+}
+
+// ListAlertEvents is used to retrieve New Relic alert events
+func (a *Alerts) ListAlertViolations(params *ListAlertViolationsParams) ([]*AlertViolation, error) {
+	alertViolations := []*AlertViolation{}
+	nextURL := a.config.Region().RestURL("alerts_violations.json")
+
+	for nextURL != "" {
+		response := alertViolationsResponse{}
+		resp, err := a.client.Get(nextURL, &params, &response)
+
+		if err != nil {
+			return nil, err
+		}
+
+		alertViolations = append(alertViolations, response.AlertViolations...)
+
+		paging := a.pager.Parse(resp)
+		nextURL = paging.Next
+	}
+
+	return alertViolations, nil
+}
+
+type alertViolationsResponse struct {
+	AlertViolations []*AlertViolation `json:"violation,omitempty"`
+}
